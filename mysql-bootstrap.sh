c2307e470aeda1c7ab8c97e0b542f5b09ab0b01f
Zhao KK <kikakkz@hotmail.com>
2021-03-17 17:42:12 +0800

    add mysql with etcd

--- mysql-bootstrap.sh
+++ mysql-bootstrap.sh
@@ -3,8 +3,7 @@
 mkdir -p /home/mysql
 service mysql start
 
-etcdctl --endpoints="http://etcd.npool.top:2379"
-etcdctl put root/mysql.npool.top "{\"host\": \"$MYSQL_HOST\", \"user\": \"root\", \"passwd\": \"123456\"}"
+etcdctl --endpoints=etcd.npool.top:2379 put root/mysql.npool.top "{\"host\":\"172.172.0.10\",\"user\":\"root\",\"passwd\":\"123456\"}"
 
 mysql -uroot -p123456 <<EOF
 source /mysql/fbc-license-db.sql;
