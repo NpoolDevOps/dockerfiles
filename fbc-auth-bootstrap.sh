c951c75d32343f609cba3d4a2bbf8ac1f3bde7ba
Zhao KK <kikakkz@hotmail.com>
2021-03-20 14:01:33 +0800

    export port

--- fbc-auth-bootstrap.sh
+++ fbc-auth-bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PORT=`grep "port" /my-repo/fbc-auth-service.conf | awk '{print $2}'`
-echo $PORT
 etcdctl --endpoints=$ENTROPY_ETCD_HOSTS put root/auth.npool.top "{\"host\":\"$ENTROPY_AUTH_HOST:$PORT\"}"
+echo $PORT
+sed -i s/\"port\".*/\"port\":\ $PORT/g  /my-repo/fbc-auth-service.conf
 
 cd /my-repo/
 export GOPROXY=https://goproxy.cn
--- fbc-license-bootstrap.sh
+++ fbc-license-bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PORT=`grep "port" /my-repo/fbc-license-service.conf | awk '{print $2}'`
-echo $PORT
 etcdctl --endpoints=$ENTROPY_ETCD_HOSTS put root/license.npool.top "{\"host\":\"$ENTROPY_LICENSE_HOST:$PORT\"}"
+
+echo $PORT
+sed -i s/\"port\".*/\"port\":\ $PORT/g  /my-repo/fbc-license-service.conf
 
 cd /my-repo/
 export GOPROXY=https://goproxy.cn
