7983c20f4a4fc72d228168f9926e4d4293417d7f
Zhao KK <kikakkz@hotmail.com>
2021-04-21 01:00:15 +0800

    install idc prometheus

--- prometheus-bootstrap.sh
+++ prometheus-bootstrap.sh
@@ -7,14 +7,35 @@ etcdctl --endpoints=$ENTROPY_ETCD_HOSTS put root/prometheus.npool.top "{\"host\"
 ENV_WORKSPACE=/root/workspace
 DIR=/usr/local
 PRO_LOG_FILE=/tmp/prometheus.log
+DEVOPS_LOG_FILE=/tmp/fbc-devops.log
+IP=`hostname -I`
+prover=prometheus-2.20.1
+protar=$prover.linux-amd64.tar.gz
+prodir=$DIR/prometheus
+FBC_DEVOPS=fbc-devops-peer
+FBC_LICENSE=fbc-license
 
 echo "Get command parameter" > $PRO_LOG_FILE
+echo "Get command parameter" > $DEVOPS_LOG_FILE
 mkdir -p $ENV_WORKSPACE
 yum install curl -y >> $PRO_LOG_FILE
 
+function error() {
+  echo "  <E> -> $1 ~"
+  exit 1
+}
+
+function warn() {
+  echo "  <W> -> $1 ~"
+}
+
+function info() {
+  echo "  <I> -> $1 ~"
+}
+
 function reliable_download() {
   while true; do
-    echo "Try to download $1 at $2" >> $PRO_LOG_FILE
+    info "Try to download $1 at $2" >> $PRO_LOG_FILE
     curl -L $1 -o $2
     [ ! $? -eq 0 ] && warn "Cannot download $1" && continue
     break
@@ -26,30 +47,68 @@ function file_sha256sum() {
 }
 
 function install_prometheus_service() {
-  prover=prometheus-2.20.1
-  protar=$prover.linux-amd64.tar.gz
-  prodir=$DIR/prometheus
   prohash=8fb248b579b8b9a88dd9b1686f7f68db7071960a45b28619145d3a4037375dcb
   [ -f $ENV_WORKSPACE/$protar ] && sum=`sha256sum $ENV_WORKSPACE/$protar`
   if [ "x$sum" == "x$prohash" ]; then
-    echo "$protar is already downloaded at $ENV_WORKSPACE" >> $PRO_LOG_FILE
+    info "$protar is already downloaded at $ENV_WORKSPACE" >> $PRO_LOG_FILE
   else
-    echo "Download $protar to $ENV_WORKSPACE" >> $PRO_LOG_FILE
-    reliable_download  http://$DOWNLOAD_IMG_HOST/$protar $ENV_WORKSPACE/$protar
+    info "Download $protar to $ENV_WORKSPACE" >> $PRO_LOG_FILE
+    ALL_PROXY= reliable_download  http://$DOWNLOAD_IMG_HOST/$protar $ENV_WORKSPACE/$protar
     sum=`sha256sum $ENV_WORKSPACE/$protar`
   fi
-  echo "Install prometheus..." >> $PRO_LOG_FILE
+  info "Install prometheus..." >> $PRO_LOG_FILE
   cd $ENV_WORKSPACE
   tar xf $protar >> $PRO_LOG_FILE
-  ls >> $PRO_LOG_FILE
   mkdir -p $prodir
   cp -r $prover.linux-amd64/* $prodir/
   mkdir -p $prodir/rules
   mv /alert-rules.yml $prodir/rules/
-  ls $prodir/rules/
+}
+
+function install_devops_peer() {
+  rm -rf $ENV_WORKSPACE/$FBC_DEVOPS
+  rm -rf $ENV_WORKSPACE/$FBC_LICENSE
+
+  echo $SOCKS > ~/.git-credentials
+  echo $GITINFO >> ~/.git-credentials
+  info "clone... $FBC_LICENSE" >> $DEVOPS_LOG_FILE
+  git config --global credential.helper store
+  git clone https://github.com/NpoolDevOps/$FBC_LICENSE.git $ENV_WORKSPACE/$FBC_LICENSE
+  if [ ! $? -eq 0 ]; then
+    error "CANNOT clone $FBC_LICENSE" >> $DEVOPS_LOG_FILE
+  fi
+
+  info "clone... $FBC_DEVOPS" >> $DEVOPS_LOG_FILE
+  git clone https://github.com/NpoolDevOps/$FBC_DEVOPS.git $ENV_WORKSPACE/$FBC_DEVOPS
+  if [ ! $? -eq 0 ]; then
+    error "CANNOT clone $FBC_DEVOPS" >> $DEVOPS_LOG_FILE
+  else
+    cd $ENV_WORKSPACE/$FBC_DEVOPS
+    export GOPROXY=https://goproxy.cn
+    go build
+    if [ ! 0 -eq $? ]; then
+      install_go
+      export GOPROXY=https://goproxy.cn
+      go build
+    fi
+  fi
+}
+
+function install_idc_devops() {
+  IDC=$IDC
+  info "IDC ==  $IDC" >> $DEVOPS_LOG_FILE
+  if [ "x$IDC" == "xtrue" ]; then
+    info "install devops peer" >> $DEVOPS_LOG_FILE
+    install_devops_peer
+    cd $ENV_WORKSPACE/$FBC_DEVOPS
+    ./$FBC_DEVOPS --main-role=gateway --network-type=filecoin --username=$USERNAME --password=$PASSWORD --test-mode=true --snmp-monitor=true --snmp-user=$SNMP_USER --snmp-pass=$SNMP_PASS --snmp-target=$SNMP_TARGET --snmp-community=$SNMP_COMMUNITY --location-label=$LABLE --mon-address=$IP >> $DEVOPS_LOG_FILE 2>&1 &
+  fi
 }
 
 
 install_prometheus_service
-/usr/local/prometheus/prometheus --config.file=/usr/local/prometheus/prometheus.yml --storage.tsdb.path=/usr/local/prometheus/data --web.enable-lifecycle >> $PRO_LOG_FILE 2>&1
+install_idc_devops
+
+sleep 30
+$prodir/prometheus --config.file=$prodir/prometheus.yml --storage.tsdb.path=$prodir/data --web.enable-lifecycle >> $PRO_LOG_FILE 2>&1
 
