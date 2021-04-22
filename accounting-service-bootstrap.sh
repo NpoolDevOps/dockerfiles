ab695c9d31bc743a92ad71ea969e24c6dfcbc461
entropy <sc@sc.com>
2021-04-22 15:26:12 +0000

    expose public ip

--- accounting-service-bootstrap.sh
+++ accounting-service-bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sed -i s/\"port\".*/\"port\":\ $PORT/g  /my-repo/fbc-accounting-service.conf
+sed -i s/\"port\".*/\"port\":\ $PORT,/g  /my-repo/fbc-accounting-service.conf
 sed -i s/\"passwd\".*/\"passwd\":\"$PASSWD\",/g  /my-repo/fbc-accounting-service.conf
 sed -i s/\"host\".*/\"host\":\"$HOST\",/g  /my-repo/fbc-accounting-service.conf
+sed -i s#\"rpc\".*#\"rpc\":\"http:\/\/$LOTUS:1234\/rpc\/v0\"\#g /my-repo/fbc-accounting-service.conf
+sed -i s/\"ip\".*/\"ip\":\"$PUBLIC_IP\",/g /my-repo/fbc-accounting-service.conf
 cat /my-repo/fbc-accounting-service.conf
 
 cd /my-repo/
