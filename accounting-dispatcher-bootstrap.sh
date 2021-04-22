5d142078840ae7d84fbd52b9dc24e8f9974f5e8f
Zhao KK <kikakkz@hotmail.com>
2021-04-22 17:27:33 +0800

    install accounting dispatcher

---
+++ accounting-dispatcher-bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+etcdctl --endpoints=$ENTROPY_ETCD_HOSTS put root/dispatcher.npool.top "{\"host\":\"$ENTROPY_DISPATCHER_HOST:$PORT\"}"
+echo $PORT
+sed -i s/\"port\".*/\"port\":\ $PORT/g  /my-repo/accounting-dispatcher.conf
+
+cd /my-repo/
+export GOPROXY=https://goproxy.cn
+go build
+./service-register >> /tmp/accounting-dispatcher.log 2>&1
